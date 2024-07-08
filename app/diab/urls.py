c38f968897d3cc5f1281f6e7369cd635abb1c863
Jovialngandu <jovialngandu2@gmail.com>
2024-07-09 01:28:41 +0200

    [UPDATE] Ajout des  liens d'URL de nos pages

--- app/diab/urls.py
+++ app/diab/urls.py
@@ -6,4 +6,12 @@ from . import views
 app_name = "diab"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login", views.login, name="login"),
+    path("signin", views.signin, name="signin"),
+    path("medecin", views.medecin, name="medecin"),
+    path("patients", views.patients, name="patients"),
+    path("dossier", views.dossier, name="dossier"),
+    path("profile", views.profile, name="profile"),
+    path("historic_consult", views.historic_consult, name="historic_consult"),
+    
 ]
