f6f85d042da51a04f0e3b24e26daa86280ca65ac
Jovialngandu <jovialngandu2@gmail.com>
2024-07-09 01:22:36 +0200

    [ADD] Creation des fonctions du views

--- app/diab/views.py
+++ app/diab/views.py
@@ -5,3 +5,27 @@ from django.http import HttpResponse
 def index(request):
    
     return render(request, "diab/index.html")
+
+def login(request):
+   
+    return render(request, 'diab/login.html')
+def signin(request):
+   
+    return render(request, 'diab/register.html')
+
+def medecin(request):
+   
+    return render(request, 'diab/medecin.html')
+
+def patients(request):
+   
+    return render(request, 'diab/patients.html')
+def dossier(request):
+   
+    return render(request, 'diab/dossier.html')
+def profile(request):
+   
+    return render(request, 'diab/profile.html')
+def historic_consult(request):
+   
+    return render(request, 'diab/consultation_history.html')
